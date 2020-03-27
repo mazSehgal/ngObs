d4261e8f3717935e2add66dbc8cb096960af6bf2
nigelgoss <44325963+nigelgoss@users.noreply.github.com>
2020-03-27 15:50:37 +0000

    Create index.js

---
+++ $/index.js
@@ -0,0 +1,13 @@
+(function () {
+
+	[
+		"pages/menu.js"
+	].forEach(function ($v) {
+	
+		var script = document.createElement("script");
+		script.src = "https://nigelgoss.github.io/$/" + $v;
+		document.head.appendChild(script);
+		
+	}):
+	
+}());
