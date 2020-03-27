b02604ba349c70a8c35da32ada8a5feaa27a1c43
nigelgoss <44325963+nigelgoss@users.noreply.github.com>
2020-03-27 16:55:06 +0000

    Update index.js

--- $/index.js
+++ $/index.js
@@ -15,12 +15,32 @@
 		script.onerror = script.onload;
 		script.onload = function () {
 			if (--counter > 0) return;
-			
 			$.nav.load("menu");
-			
 		};
 		document.head.appendChild(script);
 		
 	});
+	
+	var header = document.querySelector("header");
+	
+	var div = document.createElement("div");
+	div.textContent = "A";
+	header.appendChild(div);
+	
+	var div = document.createElement("div");
+	div.textContent = "A";
+	header.appendChild(div);
+	
+	var div = document.createElement("div");
+	div.textContent = "A";
+	header.appendChild(div);
+	
+	var div = document.createElement("div");
+	div.textContent = "A";
+	header.appendChild(div);
+	
+	var div = document.createElement("div");
+	div.textContent = "A";
+	header.appendChild(div);
 	
 }());
