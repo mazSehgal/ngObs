6421d368ece845e46604ddf5a8a17848e0e222ec
nigelgoss <44325963+nigelgoss@users.noreply.github.com>
2020-03-27 21:17:21 +0000

    Update database.js

--- $/modules/database.js
+++ $/modules/database.js
@@ -10,10 +10,14 @@ $.db = (function () {
 		xhr.onreadystatechange = function () {
 			if (xhr.readyState !== 4) return;
 			if (xhr.status !== 200) return;
-			await delay(1000);
 			$ftn(xhr.responseText);
 		};
+		
+		//xhr.open("POST", $.const.root + "!/api.php", true);
+		//xhr.send(JSON.stringify($in));
+		
 		xhr.open("GET", $.const.root + "!/" + $in[0] + ".dat", true);
+		await delay(1000);
 		xhr.send();
 		
 	};
