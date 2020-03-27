7b0c1d226d679d20fba0bb6cffd772c862c13443
nigelgoss <44325963+nigelgoss@users.noreply.github.com>
2020-03-27 19:19:55 +0000

    Update sign_in.js

--- $/pages/sign_in.js
+++ $/pages/sign_in.js
@@ -1,62 +1,67 @@
-(function () {
+$.pages["Sign In"] = (function () {
 
-  // ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- -----
+// ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- -----
 
-  function forward () {
-    back();
-  };
-  
-  function back () {
-    document.querySelector("main").replaceWith(main);
+function forward () {
+	back();
+};
+
+function back () {
+	document.querySelector("main").replaceWith(main);
+};
+
+// ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- -----
+
+var main = document.createElement("main");
+
+var username = document.createElement("input");
+main.appendChild(username);
+username.type = "text";
+username.value = "test";
+
+var password = document.createElement("input");
+main.appendChild(password);
+password.type = "password";
+password.value = "test";
+
+var buttonSignIn = document.createElement("button");
+main.appendChild(buttonSignIn);
+buttonSignIn.textContent = "Sign In";
+buttonSignIn.onpointerdown = function () {
+	alert(username.value + " " + password.value);
+};
+
+// ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- -----
+
+var sso = document.querySelector("#sso");
+
+var counter = 10;
+
+function ssoTry () {
+
+	var user = sso.querySelector("#user").value;
+	var pass = sso.querySelector("#pass").value;
+	if (user !== "" && pass !== "") {
+		username.value = user;
+		password.value = pass;
+		buttonSignIn.onpointerdown();
 	};
-  
-  // ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- -----
-  
-  var main = document.createElement("main");
-  
-  var username = document.createElement("input");
-  main.appendChild(username);
-  username.type = "text";
-  username.value = "test";
-  
-  var password = document.createElement("input");
-  main.appendChild(password);
-  password.type = "password";
-  password.value = "test";
-  
-  var buttonSignIn = document.createElement("button");
-  main.appendChild(buttonSignIn);
-  buttonSignIn.textContent = "Sign In";
-  buttonSignIn.onpointerdown = function () {
-    alert(username.value + " " + password.value);
-  };
-  
-  // ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- -----
 
-	var sso = document.querySelector("#sso");
+	if (--counter === 0) {
+		sso.parentNode.removeChild(sso);
+	} else {
+		setTimeout(ssoTry, 200);
+	};
 
-	var counter = 10;
+};
+
+ssoTry();
+
+// ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- -----
+
+return {
+	"forward": forward,
+	"back": back,
+};
 	
-	function ssoTry () {
-		
-		var user = sso.querySelector("#user").value;
-		var pass = sso.querySelector("#pass").value;
-		if (user !== "" && pass !== "") {
-			username.value = user;
-			password.value = pass;
-			buttonSignIn.onpointerdown();
-		};
-		
-		if (--counter === 0) {
-			sso.parentNode.removeChild(sso);
-		} else {
-			setTimeout(ssoTry, 200);
-		};
-		
-	};
-
-	ssoTry();
-  
-  // ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- -----
-
-}())
+}());
