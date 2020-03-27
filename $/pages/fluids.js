9d75a37653b1fed041f8890b37b1c47305c03527
nigelgoss <44325963+nigelgoss@users.noreply.github.com>
2020-03-27 23:09:51 +0000

    Create fluids.js

---
+++ $/pages/fluids.js
@@ -0,0 +1,18 @@
+$.pages["Fluids"] = (function () {
+
+  function forward () {
+  
+  };
+
+  function back () {
+  
+  };
+
+  var main = Ext["Fluids"];
+  
+  return {
+    "forward": forward,
+    "back": back,
+  };
+  
+}());
