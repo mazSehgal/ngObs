93c8eb62effbbb45ffd5b609aa725ddf43a9c19c
nigelgoss <44325963+nigelgoss@users.noreply.github.com>
2020-03-27 18:00:08 +0000

    Update navigate.js

--- $/modules/navigate.js
+++ $/modules/navigate.js
@@ -4,11 +4,13 @@
 	
 	function history ($page) {
 		
+		var rtn = null;
+		
 		if ($page !== undefined) {
 			historyList.push($page);
 		} else if (historyList.length > 2) {
 			historyList.pop();
-			var rtn = historyList.slice(-1)[0];
+			rtn = historyList.slice(-1)[0];
 		};
 		
 		document.querySelector("#buttonBack").style.visibility = (historyList.length > 2) ? "visible" : "hidden";
@@ -23,7 +25,8 @@
 	};
 	
 	function back () {
-		if (var page = history() === undefined) return;
+		var page = history();
+		if (page === null) return;
 		$.pages[page].back();
 	};
 	
