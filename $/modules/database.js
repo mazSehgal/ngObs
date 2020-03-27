01345491e143a71432cb2352fb52bf8eee86694c
nigelgoss <44325963+nigelgoss@users.noreply.github.com>
2020-03-27 21:00:13 +0000

    Update database.js

--- $/modules/database.js
+++ $/modules/database.js
@@ -14,8 +14,8 @@ $.db = (function () {
 			if (xhr.status !== 200) return;
 			$ftn(xhr.responseText);
 		};
-		xhr.open("POST", $.const.root + "!/" + $in[0] + ".txt", true);
-		xhr.send(JSON.stringify($in));
+		xhr.open("GET", $.const.root + "!/" + $in[0] + ".txt", true);
+		xhr.send();
 		
 	};
 	
