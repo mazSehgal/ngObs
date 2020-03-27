6e75974c3c0a24e0b7c4fff43ca531c662add7f1
nigelgoss <44325963+nigelgoss@users.noreply.github.com>
2020-03-27 20:40:56 +0000

    Update database.js

--- $/modules/database.js
+++ $/modules/database.js
@@ -4,9 +4,9 @@ $.db = (function () {
 		return new Promise(function (res) { setTimeout(res, $ms); });
 	};
 	
-	async function query () {
+	async function query ($in, $ftn) {
 		await delay(1000);
-		return {"A":1, "B":2, "C":3};
+		$ftn({"A":1, "B":2, "C":3});
 	};
 	
 	return {
