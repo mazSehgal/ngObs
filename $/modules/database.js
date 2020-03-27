c6ce17eafe411f2cde049b5e838d270c9242ce68
nigelgoss <44325963+nigelgoss@users.noreply.github.com>
2020-03-27 20:54:31 +0000

    Update database.js

--- $/modules/database.js
+++ $/modules/database.js
@@ -5,8 +5,18 @@ $.db = (function () {
 	};
 	
 	async function query ($in, $ftn) {
+		
 		await delay(1000);
-		$ftn({"A":1, "B":2, "C":3});
+		
+		var xhr = new XMLHttpRequest();
+		xhr.onreadystatechange = function () {
+			if (xhr.readyState !== 4) return;
+			if (xhr.status !== 200) return;
+			$ftn(xhr.responseText);
+		};
+		xhr.open("POST", $.const.root + "!/" + $in[0] + ".js", true);
+		xhr.send(JSON.stringify($in));
+		
 	};
 	
 	return {
