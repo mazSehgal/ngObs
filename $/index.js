f30feaf21ce658e4d0d057b1f65a6592afdb9f8a
nigelgoss <44325963+nigelgoss@users.noreply.github.com>
2020-03-27 16:19:20 +0000

    Update index.js

--- $/index.js
+++ $/index.js
@@ -2,12 +2,23 @@
 
 	$.pages = {};
 	
-	[
-		"pages/menu.js"
-	].forEach(function ($v) {
+	var loadList = [
+		"modules/navigate.js",
+		"pages/menu.js",
+	];
+	
+	var counter = loadList.length;
+	loadList.forEach(function ($v) {
 	
 		var script = document.createElement("script");
 		script.src = $.const.root + "$/" + $v;
+		script.onerror = script.onload;
+		script.onload = function () {
+			if (--counter > 0) return;
+			
+			$.nav.load("menu");
+			
+		};
 		document.head.appendChild(script);
 		
 	});
