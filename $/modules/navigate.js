e087003c64b623a4701af56f7f3020f629da7a96
nigelgoss <44325963+nigelgoss@users.noreply.github.com>
2020-03-27 16:49:04 +0000

    Update navigate.js

--- $/modules/navigate.js
+++ $/modules/navigate.js
@@ -1,11 +1,21 @@
 (function () {
-
+	
+	var history = [];
+	
 	function load ($page) {
+		history.push($page);
 		$.pages[$page].forward();
 	};
 	
+	function back () {
+		if (history.length <= 2) return;
+		history.pop();
+		$.pages[history.slice(-1)[0]].back();
+	};
+	
 	$.nav = {
-		"load": load
+		"load": load,
+		"back": back
 	};
 	
 }());
