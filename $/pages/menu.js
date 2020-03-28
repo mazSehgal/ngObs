9abaf825f3fbe31622fbfce883284694bb34bfc1
nigelgoss <44325963+nigelgoss@users.noreply.github.com>
2020-03-28 21:10:25 +0000

    Update menu.js

--- $/pages/menu.js
+++ $/pages/menu.js
@@ -9,13 +9,18 @@ $.pages["Menu"] = (function () {
 	};
 	
 	var main = document.createElement("main");
+	main.style.flex = "1 1 auto";
 	main.style.display = "flex";
 	main.style.flexDirection = "column";
+
+	var div = document.createElement("div");
+	main.appendChild(div);
+	div.style.flex = "1 1 auto";
 	
 	[
 		["Fluids", "Fluid Balance Chart"],
 	].forEach(function ($v) {
-	
+
 		var button = document.createElement("button");
 		button.textContent = $v[1];
 		button.style.flex = "1 0 50px";
@@ -23,12 +28,17 @@ $.pages["Menu"] = (function () {
 		button.onpointerdown = function () {
 			$.nav.load($v[0]);
 		};
-		main.appendChild(button);
-		
+		div.appendChild(button);
+
 	});
+
+	var div = document.createElement("div");
+	main.appendChild(div);
+	div.style.flex = "0 0 auto";
+	div.style.display = "flex";
 	
 	var button = document.createElement("button");
-	main.appendChild(button);
+	div.appendChild(button);
 	button.textContent = "Sign Out";
 	button.onpointerdown = function () {
 		onbeforeunload = null;
