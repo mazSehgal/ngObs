e40e120fa7f6bf1dc03079e6f724aa3d292da126
nigelgoss <44325963+nigelgoss@users.noreply.github.com>
2020-03-28 14:23:20 +0000

    Update menu.js

--- $/pages/menu.js
+++ $/pages/menu.js
@@ -13,18 +13,16 @@ $.pages["Menu"] = (function () {
 	main.style.flexDirection = "column";
 	
 	[
-		["Fluids Chart", "Fluids"],
-		["Fluids", "Fluids"],
-		["Fluids", "Fluids"],
+		["Fluids", "Fluid Balance Chart"],
 	].forEach(function ($v) {
 	
 		var button = document.createElement("button");
-		button.textContent = $v[0];
+		button.textContent = $v[1];
 		button.style.flex = "1 0 50px";
 		button.style.margin = "1px";
 		button.style.padding = "10px";
 		button.onpointerdown = function () {
-			$.nav.load($v[1]);
+			$.nav.load($v[0]);
 		};
 		main.appendChild(button);
 		
