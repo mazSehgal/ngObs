b2ae54d9fde477505277d74aa0c1d2610936c375
nigelgoss <44325963+nigelgoss@users.noreply.github.com>
2020-03-29 15:26:25 +0100

    Update datagrid.js

--- $/modules/datagrid.js
+++ $/modules/datagrid.js
@@ -1,38 +1,42 @@
 $.datagrid = (function () {
 
-var store = {
-	"ftn": null,
-	"data": null
-};
+	return function () {
+
+		var store = {
+			"ftn": null,
+			"data": null
+		};
   
-var section = document.createElement("section");
+		var section = document.createElement("section");
 
-Object.defineProperties(section, {
-	
-	"ftn": {
-		get: function () { return store.ftn; },
-		set: function ($in) {	store.data = $in; build(); }
-	},
-	
-	"data": {
-		get: function () { return store.data; },
-		set: function ($in) {	store.data = $in; build(); }
-	},
+		Object.defineProperties(section, {
 
-});
+			"ftn": {
+				get: function () { return store.ftn; },
+				set: function ($in) {	store.data = $in; build(); }
+			},
 
-function build () {
+			"data": {
+				get: function () { return store.data; },
+				set: function ($in) {	store.data = $in; build(); }
+			},
 
-	section.textContent = "";
+		});
 
-	if (store.ftn === null || store.data === null) return; 
+		function build () {
+
+			section.textContent = "";
+
+			if (store.ftn === null || store.data === null) return; 
+
+			store.data.forEach(function ($v) {
+				section.appendChild(store.ftn($v));
+			});
 	
-	store.data.forEach(function ($v) {
-		section.appendChild(store.ftn($v));
-	});
+		};
 	
-};
-	
-return section;
+		return section;
+		
+	};
 
 }());
