bed9fc2d777c8ffba38c85ac2272d7b7d24e899a
nigelgoss <44325963+nigelgoss@users.noreply.github.com>
2020-03-31 15:59:22 +0100

    Create pain_assessment.js

---
+++ $/pages/pain_assessment.js
@@ -0,0 +1,25 @@
+$.pages["Pain Assessment"] = (function () {
+
+	function forward () {
+		back();
+	};
+	
+	function back () {
+		document.querySelector("main").replaceWith(main);
+	};
+	
+	var main = document.createElement("main");
+	
+	var button = document.createElement("button");
+	main.appendChild(button);
+	button.textContent = "Go";
+	button.onpointerdown = function () {
+		//$.nav.load("Search Results");
+	};
+	
+	return {
+		"forward": forward,
+		"back": back,
+	};
+
+}());
