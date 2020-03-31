591b75fa14354362de81d890f28e11a002186ed3
nigelgoss <44325963+nigelgoss@users.noreply.github.com>
2020-03-31 16:32:37 +0100

    Update patient_banner.js

--- $/modules/patient_banner.js
+++ $/modules/patient_banner.js
@@ -1,3 +1,30 @@
-	function patientBanner ($show) {
-		document.querySelector("#patientBanner").style.display = ($show === true) ? "flex" : "none";
+	
+$.patientBanner = (function () {
+	
+	var store = {};
+	
+	var patientBanner = document.querySelector("#patientBanner");
+	
+	function showBanner ($show) {
+		patientBanner.style.display = ($show === true) ? "flex" : "none";
 	};
+	
+	function build ($in) {
+		patientBanner.textContent = $in;
+	};
+	
+	Object.defineProperties(patientBanner, {
+		
+		"data": {
+			"get": function () { return store; },
+			"set": function ($in) { store = $in; build(); }
+		}
+		
+	});
+	
+	patientBanner.show = function () { showBanner(true); };
+	patientBanner.hide = function () { showBanner(false); };
+	
+	return patientBanner;
+	
+}());
