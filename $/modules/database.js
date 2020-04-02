659107e211433c487453ecaff79b96867276c2fc
nigelgoss <44325963+nigelgoss@users.noreply.github.com>
2020-04-02 15:53:44 +0100

    Update database.js

--- $/modules/database.js
+++ $/modules/database.js
@@ -7,6 +7,7 @@ $.db = (function () {
 			if (xhr.readyState !== 4) return;
 			if (xhr.status !== 200) return;
 			$ftn(JSON.parse(xhr.responseText));
+			spinnerScreen.parentNode.removeChild(spinnerScreen);
 		};
 		
 		//xhr.open("POST", $.const.root + "!/api.php", true);
@@ -14,8 +15,29 @@ $.db = (function () {
 		
 		xhr.open("GET", $.const.root + "!/" + $in[0] + ".dat", true);
 		xhr.send();
+		document.body.appendChild(spinnerScreen);
 		
 	};
+	
+	var spinnerScreen = document.createElement("div");
+	spinnerScreen.style.position = "absolute";
+	spinnerScreen.style.top = "0";
+	spinnerScreen.style.left = "0";
+	spinnerScreen.style.width = "100%";
+	spinnerScreen.style.height = "100%";
+	spinnerScreen.onpointerdown = function () {
+		event.preventDefault();
+	};
+	
+		var div = document.createElement("div"); spinnerScreen.appendChild(div);
+		div.style.position = "absolute";
+		div.style.top = "50%";
+		div.style.left = "50%";
+		div.style.transform = "translate(-50%,-50%)";
+			
+			var img = document.createElement("img"); div.appendChild(img);
+			img.src = $.const.root + "resources/spinner.png";
+			img.style.animation = "spin 1s linear infinite";
 	
 	return {
 		"query": query,
