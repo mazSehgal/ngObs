5ca5f5a80ae1298f84647748b2f439d9e03ec690
nigelgoss <44325963+nigelgoss@users.noreply.github.com>
2020-04-04 23:51:00 +0100

    Update navigate.js

--- $/modules/navigate.js
+++ $/modules/navigate.js
@@ -1,29 +1,25 @@
-(function () {
+$.nav = (function () {
 	
 	var historyList = [];
 	
-	var last;
-	setInterval(function () {
-		if (last === historyList.slice(-1)[0]) return;
-		last = historyList.slice(-1)[0];
+	function update () {
 		document.querySelector("#pageTitle").textContent = "ngObs :: " + historyList.slice(-1)[0];	
 		document.querySelector("#buttonBack").disabled = (historyList.length > 2) ? false : true;
 		document.querySelector("#buttonMenu").disabled = (historyList.length > 2) ? false : true;
-	}, 333);
+	};
 	
-	function load ($page, $in) {
-		historyList.push($page);
-		$.pages[$page].forward($in);
+	function load ($page, $input) {
+		$.pages[$page].forward($input, function () { historyList.push($page); update(); });
 	};
 	
 	function back ($page) {
 		if (historyList.length < 3) return;
 		historyList.pop();
-		$.pages[historyList.pop()].back();
+		$.pages[historyList.slice(-1)[0]].back(function () { update(); });
 		if ($page !== undefined && $page !== historyList.slice(-1)[0]) back($page);
 	};
 	
-	$.nav = {
+	return {
 		"load": load,
 		"back": back,
 	};
