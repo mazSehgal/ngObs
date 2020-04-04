a3dfb5d08aa5603fd8d747e1e78ffbb1f60267b3
nigelgoss <44325963+nigelgoss@users.noreply.github.com>
2020-04-04 21:06:38 +0100

    Create pressure_ulcers_add.js

---
+++ $/pages/pressure_ulcers_add.js
@@ -0,0 +1,61 @@
+$.pages["Pressure Ulcers Add"] = (function () {
+
+	function forward () {
+		back();
+	});
+	
+	function back () {
+		document.querySelector("main").replaceWith(main);
+	};
+	
+	var store = {};
+	
+	var main = document.createElement("main");
+	
+	var button = document.createElement("button");
+	main.appendChild(button);
+	button.textContent = "Snap";
+	button.ngpointerdown = function () {
+		
+		navigator.camera.getPicture(
+			
+			function success ($imageData) {
+        		store.image = $imageData;
+				build();
+			},
+			
+			function failure ($e) {},
+			
+			{
+				quality: 100,
+				destinationType: Camera.DestinationType.DATA_URL,
+				sourceType: Camera.PictureSourceType.CAMERA,
+				encodingType: Camera.EncodingType.PNG,
+				mediaType: Camera.MediaType.PICTURE,
+				allowEdit: false,
+				correctOrientation: true
+			}
+			
+		);
+		
+	};
+	
+	var img = document.createElement("img");
+	main.appendChild(img);
+	img.style.maxWidth = "300px";
+	img.style.maxHeight = "300px";
+			
+	function build () {
+		img.src = "data:image/png;base64," + store.image;
+	}; 
+	
+	var button = document.createElement("button");
+	main.appendChild(button);
+	button.textContent = "Save";
+	
+	return {
+		"forward": forward,
+		"back": back,
+	};
+  
+}());
