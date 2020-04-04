70ef635a503dbc512f52636567a6956481b5bc6e
nigelgoss <44325963+nigelgoss@users.noreply.github.com>
2020-04-04 23:41:12 +0100

    Update navigate.js

--- $/modules/navigate.js
+++ $/modules/navigate.js
@@ -2,36 +2,26 @@
 	
 	var historyList = [];
 	
-	function history ($page) {
-		
-		if ($page !== undefined) {
-			historyList.push($page);
-		} else if (historyList.length > 2) {
-			historyList.pop();
-		};
-		
-		document.querySelector("#pageTitle").textContent = "ngObs :: " + historyList.slice(-1)[0];
-		
+	var last;
+	setInterval(function () {
+		if (last === historyList.slice(-1)[0]) return;
+		last = historyList.slice(-1)[0];
+		document.querySelector("#pageTitle").textContent = "ngObs :: " + historyList.slice(-1)[0];	
 		document.querySelector("#buttonBack").disabled = (historyList.length > 2) ? false : true;
 		document.querySelector("#buttonMenu").disabled = (historyList.length > 2) ? false : true;
-		
-		return historyList.slice(-1)[0];
-	
-	};
+	}, 333);
 	
 	function load ($page, $in) {
+		historyList.push($page);
 		$.pages[$page].forward($in);
-		history($page);
 	};
 	
 	function back ($page) {
-		var page = history();
-		if (page === undefined) return;
-		$.pages[page].back();
-		if ($page !== undefined && $page !== page) back($page);
+		if (historyList.length < 3) return;
+		historyList.pop();
+		$.pages[historyList.pop()].back();
+		if ($page !== undefined && $page !== historyList.slice(-1)[0]) back($page);
 	};
-	
-	document.querySelector("main")
 	
 	$.nav = {
 		"load": load,
