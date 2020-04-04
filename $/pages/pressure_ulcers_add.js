e2b455bbe1a2d35a4770b355535160ec60fbf0d0
nigelgoss <44325963+nigelgoss@users.noreply.github.com>
2020-04-05 00:05:56 +0100

    Update pressure_ulcers_add.js

--- $/pages/pressure_ulcers_add.js
+++ $/pages/pressure_ulcers_add.js
@@ -1,11 +1,12 @@
 $.pages["Pressure Ulcers Add"] = (function () {
 
-	function forward () {
-		back();
+	function forward ($in, $cb) {
+		back($cb);
 	}
 	
-	function back () {
+	function back ($cb) {
 		document.querySelector("main").replaceWith(main);
+		$cb();
 	};
 	
 	var store = {};
