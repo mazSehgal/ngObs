99be3956b9dc789cd31ffd3cc34e1587a9259498
nigelgoss <44325963+nigelgoss@users.noreply.github.com>
2020-04-04 23:57:06 +0100

    Update fluids.js

--- $/pages/fluids.js
+++ $/pages/fluids.js
@@ -1,16 +1,16 @@
 $.pages["Fluids"] = (function () {
 
-  function forward () {
-		back();
+	function forward ($in, $cb) {
+		back($cb);
 	};
 	
-	function back () {
+	function back ($cb) {
 		document.querySelector("main").replaceWith(main);
+		$cb();
 	};
 	
 	var main = document.createElement("main");
 	main.textContent = "Fluids...";
-  
   
 	return {
 		"forward": forward,
