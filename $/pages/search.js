a6748428f06822f9ea6e62b1d75117c2ab712c8e
nigelgoss <44325963+nigelgoss@users.noreply.github.com>
2020-04-05 12:49:18 +0100

    Update search.js

--- $/pages/search.js
+++ $/pages/search.js
@@ -18,33 +18,33 @@ $.pages["Search"] = (function () {
 	form.style.gridTemplateColumns = "auto auto";
 	form.style.gridGap = "10px";
 	
-	var label = document.createElement("label");
-	form.appendChild(label);
-	label.textContent = "Patient No.";
+	var div = document.createElement("div");
+	form.appendChild(div);
+	div.textContent = "Patient No.";
 	
 	var input = document.createElement("input");
 	form.appendChild(input);
 	input.type = "text";
 	
-	var label = document.createElement("label");
-	form.appendChild(label);
-	label.textContent = "Patient Name";
+	var div = document.createElement("div");
+	form.appendChild(div);
+	div.textContent = "Patient Name";
 	
 	var input = document.createElement("input");
 	form.appendChild(input);
 	input.type = "text";
 	
-	var label = document.createElement("label");
-	form.appendChild(label);
-	label.textContent = "Date of Birth";
+	var div = document.createElement("div");
+	form.appendChild(div);
+	div.textContent = "Date of Birth";
 	
 	var input = document.createElement("input");
 	form.appendChild(input);
 	input.type = "text";
 	
-	var label = document.createElement("label");
-	form.appendChild(label);
-	label.textContent = "Postcode";
+	var div = document.createElement("div");
+	form.appendChild(div);
+	div.textContent = "Postcode";
 	
 	var input = document.createElement("input");
 	form.appendChild(input);
