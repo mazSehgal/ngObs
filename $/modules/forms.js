bfecb62bffc8fde6ba3c7c94d763b8ab51132797
mazSehgal <63338450+mazSehgal@users.noreply.github.com>
2020-04-08 08:42:42 +0100

    Create forms.js

---
+++ $/modules/forms.js
@@ -0,0 +1,68 @@
+if ($ === undefined) var $ = {};
+$.forms = (function () {
+
+	var ftn = {};
+
+	// ----------------------------------------------------------------------------------------------------
+
+
+	ftn.completeForm = function ($section, $data) {
+
+		for(var field in $data){
+			var ele = $section.querySelector("*[id='"+field+"']");
+			if(ele !== null) ele.value = $data[field];
+		};
+		
+	}
+
+	// ----------------------------------------------------------------------------------------------------
+
+	ftn.completeForm = function ($section, $data) {
+		Object.keys($data).forEach(function ($v) {
+			$section.querySelectorAll("*[name='"+$v+"']").forEach(function ($v2) {
+				if ($v2.tagName.toLowerCase() === "input" && $v2.type === "text") {	$v2.value = $data[$v]; }
+				else if ($v2.tagName.toLowerCase() === "textarea") { $v2.value = ($data[$v] === null) ? "" : $data[$v]; }
+				else if ($v2.tagName.toLowerCase() === "input" && $v2.type === "radio" && $v2.value === $data[$v]) { $v2.checked = true; }
+				else if ($v2.tagName.toLowerCase() === "input" && $v2.type === "checkbox" && $data[$v].indexOf($v2.value) > -1) { $v2.checked = true; }
+				else if ($v2.hasAttribute("ng-formelement") === true) { $v2.value = $data[$v]; }
+			});
+		});
+	};
+
+	ftn.serialize = function ($section) {
+		
+		var data = {};
+		
+		$section.querySelectorAll("*:not([ng-formelement]) input[type='text'][name]").forEach(function ($v) {
+			data[$v.name] = ($v.value === "") ? null : $v.value;
+		});
+		
+		$section.querySelectorAll("*:not([ng-formelement]) textarea[name]").forEach(function ($v) {
+			data[$v.name] = ($v.value === "") ? null : $v.value;
+		});
+		
+		$section.querySelectorAll("*:not([ng-formelement]) input[type='radio'][name]").forEach(function ($v) {
+			if (data[$v.name] === undefined) data[$v.name] = null;
+			if ($v.checked === true) data[$v.name] = $v.value;
+		});
+		
+		$section.querySelectorAll("*:not([ng-formelement]) input[type='checkbox'][name]").forEach(function ($v) {
+			if (data[$v.name] === undefined) data[$v.name] = [];
+			if ($v.checked === true) data[$v.name].push($v.value);
+		});
+		
+		$section.querySelectorAll("*[ng-formelement][name]").forEach(function ($v) {
+			data[$v.getAttribute("name")] = $v.value;
+		});
+		
+		return data;
+		
+	};
+  
+	// ----------------------------------------------------------------------------------------------------
+	
+	return ftn;
+	
+	// ----------------------------------------------------------------------------------------------------
+
+}());
